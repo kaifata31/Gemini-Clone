cd63c9901a1e5dc08bbe0f790285892f640bc563
Kaif <kaifata31@gmail.com>
2024-03-27 16:36:37 +0530

    Made it for giving response for prompt

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import ContextProvider from "./context/Context.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>
+  </ContextProvider>
 );
